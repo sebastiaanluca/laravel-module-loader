74f0d1757e646bda185ab0ddd415acda0f81b10a
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-08-10 16:58:50 +0200

    Dump composer autoload during test setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace SebastiaanLuca\Module\Tests;
 
 use Illuminate\Filesystem\Filesystem;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Symfony\Component\Process\Process;
 
 class TestCase extends BaseTestCase
 {
@@ -47,6 +48,10 @@ class TestCase extends BaseTestCase
         parent::setUp();
 
         app(Filesystem::class)->copyDirectory(__DIR__ . '/resources/setup', base_path());
+
+        // Generate composer autoload config base on the temp composer.json
+        // file after setting up our temporary app directory.
+        $this->dumpautoload();
     }
 
     /**
@@ -59,5 +64,15 @@ class TestCase extends BaseTestCase
         parent::tearDown();
 
         app(Filesystem::class)->deleteDirectory(base_path());
+    }
+
+    private function dumpautoload() : void
+    {
+        $process = new Process(sprintf(
+            'cd %s && composer dumpautoload',
+            base_path()
+        ));
+
+        $process->run();
     }
 }
