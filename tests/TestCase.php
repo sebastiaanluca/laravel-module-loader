73ffe6a64ec80c8bbee1b3a055e9bac68dcfe585
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-08-10 16:58:41 +0200

    Rename resource app dir

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -46,7 +46,7 @@ class TestCase extends BaseTestCase
     {
         parent::setUp();
 
-        app(Filesystem::class)->copyDirectory(__DIR__ . '/resources/examples', base_path());
+        app(Filesystem::class)->copyDirectory(__DIR__ . '/resources/setup', base_path());
     }
 
     /**
