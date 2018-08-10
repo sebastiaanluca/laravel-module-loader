d6b58b8d890d49e49eea4fde10559208800526f9
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-08-10 22:03:39 +0200

    Add cache test

---
+++ tests/Feature/ModuleLoaderCacheTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SebastiaanLuca\Module\Tests\Feature;
+
+use SebastiaanLuca\Module\Tests\TestCase;
+
+class ModuleLoaderCacheTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it loads all cached modules() : void
+    {
+        $loader = $this->getModuleLoader();
+
+        $cache = $loader->getCachePath();
+
+        $copy = copy(__DIR__ . '/../resources/cache.php', $cache);
+
+        $this->assertTrue($copy);
+        $this->assertFileExists($cache);
+
+        $loader->load();
+
+        $loaded = app()->getLoadedProviders();
+
+        $this->assertArrayHasKey('MyModule\\Providers\\MyModuleServiceProvider', $loaded);
+        $this->assertArrayNotHasKey('Another\\Providers\\AnotherServiceProvider', $loaded);
+    }
+}
