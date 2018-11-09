8d9dd22cba2c0181acad89887950e92433aa4ee7
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-11-09 19:21:20 +0100

    Test provider registration

---
+++ tests/Feature/Providers/ProviderTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SebastiaanLuca\Module\Tests\Feature\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Orchestra\Testbench\TestCase;
+use SebastiaanLuca\Module\Providers\ModuleProvider;
+use SebastiaanLuca\Module\Tests\MocksInstances;
+
+class ProviderTest extends TestCase
+{
+    use MocksInstances;
+
+    /**
+     * @test
+     */
+    public function it registers all additional providers() : void
+    {
+        $this->app->register(ProviderTestServiceProvider::class);
+
+        $this->assertArrayHasKey(
+            ProviderTestProvider::class,
+            $this->app->getLoadedProviders(),
+            'The additional provider was not registered.'
+        );
+    }
+}
+
+class ProviderTestServiceProvider extends ModuleProvider
+{
+    protected $providers = [
+        ProviderTestProvider::class,
+    ];
+}
+
+class ProviderTestProvider extends ServiceProvider
+{
+}
